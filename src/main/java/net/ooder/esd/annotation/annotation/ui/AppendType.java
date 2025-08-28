148f2582706d232d547b815189cf481c2d229480
18618287247 <18683731@qq.com>
2025-08-28 23:19:07 +0800

    将应用图标，统一到fas 支持最新的UI属性。

--- src/main/java/net/ooder/esd/annotation/annotation/ui/AppendType.java
+++ src/main/java/net/ooder/esd/annotation/annotation/ui/AppendType.java
@@ -1,6 +1,36 @@
+package net.ooder.esd.annotation.annotation.ui;
+
 import net.ooder.annotation.IconEnumstype;
 
-// ... existing code ...
 public enum AppendType implements IconEnumstype {
-    append("内部渲染", "fas fa-box"), ref("引用MODULE", "fas fa-cubes"), runtime("运行期", "fas fa-clock");
-//// ... existing code ...
+    append("内部渲染", "spafont spa-icon-c-block"), ref("引用MODULE", "spafont spa-icon-com3"), runtime("运行期", "spafont spa-icon-tools");
+
+
+    private final String name;
+
+    private final String imageClass;
+
+    AppendType(String name, String imageClass) {
+        this.name = name;
+        this.imageClass = imageClass;
+    }
+
+    public String getImageClass() {
+        return imageClass;
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
+
+    @Override
+    public String getType() {
+        return name();
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
