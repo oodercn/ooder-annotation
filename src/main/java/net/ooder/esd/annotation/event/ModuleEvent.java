2dadf44825b71dc36d4a4526abe4c06c8f1b1890
18618287247 <18683731@qq.com>
2025-10-29 22:52:16 +0800

    增加移动组件

--- src/main/java/net/ooder/esd/annotation/event/ModuleEvent.java
+++ src/main/java/net/ooder/esd/annotation/event/ModuleEvent.java
@@ -3,19 +3,22 @@ package net.ooder.esd.annotation.event;
 
 import net.ooder.esd.annotation.CustomAction;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
 public @interface ModuleEvent {
 
-    String eventName();
+    String eventName() default "";
 
     ModuleEventEnum event();
 
-    CustomAction[] actions();
+    CustomAction[] actions() default {};
 
-    String desc();
+    String desc() default "";
 
     boolean _return() default true;
 }
