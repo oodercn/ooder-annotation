803137a0ad366e346a8648b00ec4611e04898be8
18618287247 <18683731@qq.com>
2025-11-16 22:37:23 +0800

    增加移动组件

--- src/main/java/net/ooder/esd/annotation/action/CustomTarget.java
+++ src/main/java/net/ooder/esd/annotation/action/CustomTarget.java
@@ -7,14 +7,14 @@ public enum CustomTarget implements Enumstype {
     DYNCURRMODULENAME("@{CurrModule!=null?CurrModule.realClassName}"),
     DYNCURRTOPCOMPONENTNAME("@{(this.topModuleComponent!=null && this.topModuleComponent.topComponentBox!=null) ? this.topModuleComponent.topComponentBox.alias: (this.moduleComponent!=null && this.moduleComponent.topComponentBox!=null)?this.moduleComponent.topComponentBox.alias}"),
     PARENTMODULE("{page.parentModule}"),
-    DYNCOMPONENTNAME("@{this.moduleComponent.navComponent!=null?this.moduleComponent.navComponent.alias:(currComponent!=null?currComponent.alias)}"),
+    DYNCOMPONENTNAME("@{(this.moduleComponent!=null&&this.moduleComponent.navComponent!=null)?this.moduleComponent.navComponent.alias:(currComponent!=null?currComponent.alias)}"),
     TOPMODULE("@{TopModule.realClassName}"),
-    DYNADDMODULENAME("@{this.moduleComponent.addModule!=null?CurrModule.component.addModule.realClassName:CurrModule.component.editorModule}"),
-    ADDMODULEDIO("@{this.moduleComponent.addModule!=null?CurrModule.component.addModule.component.dio:true}"),
-    EDITERMODULEDIO("@{this.moduleComponent.editorModule!=null?CurrModule.component.editorModule.component.dio:true}"),
-    MOREMODULEDIO("@{this.moduleComponent.moreModule!=null?CurrModule.component.moreModule.component.dio:true}"),
-    DYNEDITORMODULETARGET("@{this.moduleComponent.editorModule!=null?this.moduleComponent.editorModule.component.target:this.moduleComponent.target}"),
-    DYNEDITORMODULENAME("@{this.moduleComponent.editorModule!=null?this.moduleComponent.editorModule.realClassName:CurrModule.realClassName}");
+    DYNADDMODULENAME("@{(this.moduleComponent!=null&&this.moduleComponent.addModule!=null)?this.moduleComponent.addModule.realClassName:this.moduleComponent.editorModule}"),
+    ADDMODULEDIO("@{(this.moduleComponent!=null&&this.moduleComponent.addModule!=null)?this.moduleComponent.addModule.component.dio:true}"),
+    EDITERMODULEDIO("@{(this.moduleComponent!=null&&this.moduleComponent.editorModule!=null)?this.moduleComponent.editorModule.component.dio:true}"),
+    MOREMODULEDIO("@{(this.moduleComponent!=null&&this.moduleComponent.moreModule!=null)?this.moduleComponent.moreModule.component.dio:true}"),
+    DYNEDITORMODULETARGET("@{(this.moduleComponent!=null&&this.moduleComponent.editorModule!=null)?this.moduleComponent.editorModule.component.target:this.moduleComponent.target}"),
+    DYNEDITORMODULENAME("@{(this.moduleComponent!=null&&this.moduleComponent.editorModule!=null)?this.moduleComponent.editorModule.realClassName:CurrModule.realClassName}");
 
     private final String name;
 
